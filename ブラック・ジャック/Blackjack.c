dff62ea4bb1e242cbd7adf7e795106728a64dff9
daishin <daishin.kawai@gmail.com>
2020-03-16 12:47:09 +0900

    トランプ仕様化

--- ブラック・ジャック/Blackjack.c
+++ ブラック・ジャック/Blackjack.c
@@ -4,23 +4,33 @@
 #include "Blackjack.h"
 
 //プロトタイプ宣言
-char* Make_trump(int*, int);       //トランプの設定
-int Takehand(int);      //手札を引く処理
-char* Judge(int, int);  //勝敗を判定して文字列を返す
+char* Make_trump(int*, int);       //送られてきた乱数をトランプに変換
+int Takehand(int);                 //手札を引く処理
+char* Judge(int, int, const int);  //勝敗を判定して文字列を返す
+
+typedef enum tag_turn{
+    PLAYER,   //自分の番
+    COMPUTER, //相手の番
+    MAX      //参加者の数
+}TURN;
 
 int main(void){
     int playerhand_sum;   //自分の手札の合計値
     int computerhand_sum; //相手の手札の合計値
+    TURN p1,p2;           //参加者
+
+    p1 = PLAYER;
+    p2 = COMPUTER;
 
     srand((unsigned int)time(NULL));
     
-    //system("clear");
+    system("clear");
     
     //プレイヤー・コンピュータの手札を引く処理
     //引数：どちらのターンか（int型）
     //戻り値：手札の合計値（int型）
-    playerhand_sum = Takehand(TURN_PLAYER);
-    computerhand_sum = Takehand(TURN_COMPUTER);
+    playerhand_sum = Takehand(p1);
+    computerhand_sum = Takehand(p2);
     
     printf("\n");
     printf("自分の手札:%d\n", playerhand_sum);
@@ -28,76 +38,72 @@ int main(void){
     printf("\n");
 
     //勝ちか負けかを判定
-    //引数：プレイヤーの合計値（int型）、コンピュータの合計値（int型）
+    //引数：プレイヤーの合計値（int型）、コンピュータの合計値（int型）、バースト値（const int型）
     //戻り値：勝敗の文字列（char型）
-    printf("%s\n", Judge(playerhand_sum, computerhand_sum));
+    printf("%s\n", Judge(playerhand_sum, computerhand_sum, BURST));
 
     return 0;
 }
 
-char* Make_trump(int* value, int hand){
-    int card[MAXCARD];
-    int kind = 0;
-    char suank[][10] = {"S", "D", "H", "C"};
+//送られてきた乱数をトランプに変換
+char* Make_trump(int* value, int number){
+    char *card;                          //戻り値用変数
+    char suank[] = {'S', 'D', 'H', 'C'}; //トランプの柄
 
-
-        if(hand >= 1 && hand <= 13){
-            kind = SPADE;
-            *value = hand;
+        if(number >= 1 && number <= 13){
+            card = &suank[SPADE];
+            *value = number;
         }
-        else if(hand >= 14 && hand <= 26){
-            kind = DIAMOND;
-            *value = hand - MAXVALUE;
+        else if(number >= 14 && number <= 26){
+            card = &suank[DIAMOND];
+            *value = number - TRUMP_MAX;
         }
-        else if(hand >= 27 && hand <= 39){
-            kind = HEART;
-            *value = hand - MAXVALUE * 2;
+        else if(number >= 27 && number <= 39){
+            card = &suank[HEART];
+            *value = number - TRUMP_MAX * 2;
         }
-        else if(hand >= 40 && hand <= 52){
-            kind = CLOVER;
-            *value = hand - MAXVALUE * 3;
+        else if(number >= 40 && number <= 52){
+            card = &suank[CLOVER];
+            *value = number - TRUMP_MAX * 3;
         }
-        return suank[kind];
+        return card;
 }
 
 //手札を引く処理
 int Takehand(int whichturn){
-    char name[2][100] = {"あなた","あいて"}; //誰の手札か判別
-    int hand_value[MAXHAND];               //引いた手札の値を保存する
-    char hand_suank[MAXHAND][10] = {};               //引いた手札の柄を保存する
-    int count = 0;                             //引いた手札の数
-    int hand = 0;                          //トランプに変換する値
-    int card_value = 0;                    //手札の値
-    char card_suank[10] = {};                    //手札の柄
-    int sum = 0;                               //手札の合計値
-    int take_hand = TRUE;                         //もう一度手札を引くかどうかの判断
+    char name[MAX][100] = {"あなた","あいて"};    //誰の手札か判別
+    char hand_suank[MAXHAND];                   //引いた手札の柄を保存する
+    int hand_value[MAXHAND];                    //引いた手札の値を保存する
+    int count = 0;                              //引いた手札の回数
+    int number = 0;                             //トランプに変換するための乱数
+    int card_value = 0;                         //手札の値
+    int sum = 0;                                //手札の合計値
+    int take_hand = TRUE;                       //もう一度手札を引くかどうかの判断
     int input = 0;
     int i;
     
     //初期化
     for(i = 0; i < MAXHAND; i++){
-        hand_suank[i] = "a";
         hand_value[i] = 0;
     }
     
     //take_handにFALSEが入るまで繰り返す
     while(take_hand == TRUE){
-        count++;
+        count++; //引いた回数追加
 
         //手札を引いて、それを手札用配列に格納
-        hand = rand() % MAXCARD;
-        hand_suank[count] = Make_trump(&card_value, hand);
+        number = rand() % TRUMPCARD;                            //トランプ変換用の乱数生成
+        hand_suank[count] = *Make_trump(&card_value, number); //トランプの柄を配列に入れる
+        hand_value[count] = card_value;                       //トランプの値を配列に入れる
 
-        printf("%s", card_suank);
-
-        hand_value[count] = card_value;
-        
-        if(card_value > LIMIT){//引いた手札が10を超えていた場合、10として計算
+        //引いた手札が10を超えていた場合、10として計算
+        if(card_value > LIMIT){
             card_value = LIMIT;
         }
-        sum += card_value;     //合計値の計算
 
-        if(sum > BURST){//合計値がバースト値を超えた場合、ループを抜ける
+        sum += card_value; //合計値の計算
+        //合計値がバースト値を超えた場合、ループを抜ける
+        if(sum > BURST){
             break;
         }
         
@@ -105,11 +111,11 @@ int Takehand(int whichturn){
         if(whichturn == 0){//自分のターンだった場合
             printf("||あなたのターンです||\n");
             printf("引いた手札：");
+            //手札を表示
             for(i = 1; i <= count; i++){
-                printf("%s%d ", hand_suank[i], hand_value[i]);
+                printf("%c%d ", hand_suank[i], hand_value[i]);
             }
             printf("\n");
-
             printf("\nもう一度引きますか？  はい:0  いいえ:1\n=>");
             scanf("%d", &input);
             system("clear");
@@ -127,8 +133,9 @@ int Takehand(int whichturn){
     }
     
     printf("%sが引いた手札：", name[whichturn]);
-    for(i = 1; i <= count; i++){//今まで引いた手札を表示
-        printf("%s%d ", hand_suank[i], hand_value[i]);
+    //手札を表示
+    for(i = 1; i <= count; i++){
+        printf("%c%d ", hand_suank[i], hand_value[i]);
     }
     printf("\n");
 
@@ -136,25 +143,31 @@ int Takehand(int whichturn){
 }
 
 //勝敗を判定して文字列を返す
-char* Judge(int player, int computer){
-    if(player > BURST){//自分がバーストした時
-        if(computer > BURST){
-            return "引き分けです。";
+char* Judge(int player, int computer, const int burst){
+    char *result;                                          //戻り値用変数
+    int burst_computer = (computer > burst);               //相手がバースト
+    int burst_player = (player > burst);                   //自分がバースト
+    int draw = (computer == player);                       //お互いが同じ数
+    int draw_burst = (player > burst && computer > burst); //お互いがバースト
+    int win_player = (player > computer);                  //自分の数が相手よりも大きい
+    int win_computer = (player < computer);                //自分の数が相手よりの小さい
+
+    if(draw || draw_burst){
+        result = "引き分けです。";
+    }
+    else if(burst_player){
+        result = "あなたの負けです...";
+    }
+    else if(burst_computer){
+        result = "あなたの勝ちです！！";
+    }
+    else{
+        if(win_player){
+            result = "あなたの勝ちです！！";
         }
-        else{
-            return "あなたの負けです...";
+        else if(win_computer){
+            result = "あなたの負けです...";
         }
     }
-    else if(player <= BURST){//自分がバーストしていない時
-        if(computer > BURST || player > computer){
-            return "あなたの勝ちです！！";
-        }
-        else if(player < computer){
-            return "あなたの負けです...";
-        }
-        else{
-            return "引き分けです。";
-        }
-    }
-    return 0;
+    return result;
 }
--- ブラック・ジャック/Blackjack.h
+++ ブラック・ジャック/Blackjack.h
@@ -4,16 +4,14 @@
 //ゲーム進行に関するもの
 #define TRUE 0
 #define FALSE 1
-#define TURN_PLAYER 0   //自分のターン
-#define TURN_COMPUTER 1 //相手のターン
 
 //手札についてのもの
-#define MAXVALUE 13     //手札として引いた値の最大値
+#define TRUMP_MAX 13    //トランプの最大値
 #define LIMIT 10        //計算する上での上限値
 #define BURST 21        //バースト値
 #define COMPUTER_GET 17 //コンピュータのドロー判断基準
 #define MAXHAND 22      //手札の最大数
-#define MAXCARD 52      //トランプのカード
+#define TRUMPCARD 52      //トランプのカード
 #define SPADE 0         //スペード
 #define DIAMOND 1       //ダイヤ
 #define HEART 2         //ハート
