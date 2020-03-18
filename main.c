c2474108d576edf0875c7a19d45365950b3e31cd
daishin <daishin.kawai@gmail.com>
2020-03-18 13:05:11 +0900

    神経衰弱途中経過...並べたカード表示まで

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "BlackJack/Blackjack.h"
+#include "NervousBreakdown/Nervousbreakdown.h"
+#include "ECS.h"
+
+int main(void){
+    //Blackjackmain();
+    //Cursor_move();
+    Nervousbreakdownmain();
+
+    return 0;
+}
