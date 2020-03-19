652bc292d099aad1fb1431c92a2ad87b89f682c9
daishin <daishin.kawai@gmail.com>
2020-03-19 17:21:10 +0900

    トランプ

--- trump.h
+++ trump.h
@@ -8,6 +8,11 @@
 #define HEART 2         //ハート
 #define CLOVER 3        //クローバー
 
+#define CARD_X 4    //配置されるカードの行数
+#define CARD_Y 13   //配置されるカードの列数
+#define SHUFFLE 100 //シャッフルする回数
+#define NONUMBER 0 //入力や表示が何もない時
+
 //送られてきた値をトランプに変換
 char* Make_trump(int*, int);
 
